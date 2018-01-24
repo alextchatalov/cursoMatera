c0d1886e16891563a9d1b7dd0a2a6cae706b058a
Alex <alex_tr1992@hotmail.com>
2018-01-24 21:24:28 -0200

    Adicionado as funções de navegar nos botoes do home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { Page } from 'ionic-angular/navigation/nav-util';
 
 @IonicPage()
 @Component({
@@ -10,6 +11,25 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  goToProfilePage() {
+    this.navCtrl.push("ProfilePage");
+  }
+
+  goToListPage() {
+    this.navCtrl.push("ListPage");
+  }
+
+  goToRelatorioPage() {
+    this.navCtrl.push("RelatorioPage");
+  }
+
+  goToScannerPage() {
+    this.navCtrl.push("ScannerPage");
+  }
+
+  goToLoginPage() {
+    this.navCtrl.push("LoginPage");
   }
 
 }
